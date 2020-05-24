ef400cf7388631c9522e5fe6317e8beae924d3b8
NathanSaldanha <Nmariano85@gmail.com>
2020-05-24 16:28:04 -0300

    Criando no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://logospng.org/wp-content/uploads/bitcoin.png"
+)
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://w7.pngwing.com/pngs/932/233/png-transparent-ethereum-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo.png"
+)
+Coin.create!(
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://pngimage.net/wp-content/uploads/2018/05/dash-coin-logo-png-4.png"
+)
